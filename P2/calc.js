2964e4fa5e95780f3468856e41554ba4933070ba
pablo arribas <pabloarribas96@hotmail.com>
2021-07-09 00:55:06 +0200

    pablo aprende a escribir bien los nombres primeri aviso...

--- P2/calc.js
+++ P2/calc.js
@@ -2,27 +2,3 @@ function numero (number){
     input = document.getElementsByClassName("display")[0];
     input.value += number;
 }
-
-function operacion (operator) {
-    
-    
-    switch (operator){
-        case '*':
-        case '/':
-            if(numbers < 1){
-                alert('Introduce a number before an operator');
-                break;
-            }
-        case '+':
-        case '-':
-           
-        case 'DEL':
-          
-        case 'AC':
-            
-        case '=':
-          
-        default:
-            break;
-    }
-}
