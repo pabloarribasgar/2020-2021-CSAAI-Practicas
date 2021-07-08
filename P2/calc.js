2cf1d1ac09990de2b13cdf5eda4e5d5ae1b8a32f
pablo arribas <pabloarribas96@hotmail.com>
2021-07-09 01:00:40 +0200

    añadiendo

--- P2/calc.js
+++ P2/calc.js
@@ -2,3 +2,28 @@ function numero (number){
     input = document.getElementsByClassName("display")[0];
     input.value += number;
 }
+
+function operacion (operator) {
+    input = document.getElementsByClassName("display")[0];
+    const lastInput = input.value.charAt(input.value.length-1);
+    
+    switch (operator){
+        case '*':
+        case '/':
+            if(numbers < 1){
+                alert('Introduce un numero antes de un operador');
+                break;
+            }
+        case '+':
+        case '-':
+           
+        case 'DEL':
+          
+        case 'AC':
+            
+        case '=':
+          
+        default:
+            break;
+    }
+}
