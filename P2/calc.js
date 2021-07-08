a9e2f70afabdad335f2a61e5b2e43848aa17bcf5
pablo arribas <pabloarribas96@hotmail.com>
2021-07-09 00:27:30 +0200

    crando functions y retoques

---
+++ P2/calc.js
@@ -0,0 +1,19 @@
+function operacion (operator) {
+    
+    switch (operator){
+        case '*':
+        case '/':
+            
+        case '+':
+        case '-':
+           
+        case 'DEL':
+          
+        case 'AC':
+            
+        case '=':
+          
+        default:
+            break;
+    }
+}
