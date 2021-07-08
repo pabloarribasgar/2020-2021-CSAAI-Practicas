735c59e6efcd00e14a75d35a3f542b9d70148382
pablo arribas <pabloarribas96@hotmail.com>
2021-07-09 00:32:56 +0200

    añadiendo clases

--- P2/calc.js
+++ P2/calc.js
@@ -1,9 +1,16 @@
 function operacion (operator) {
+    function numero (number){
+        input = document.getElementsByClassName("display")[0];
+        input.value += number;
+    }
     
     switch (operator){
         case '*':
         case '/':
-            
+            if(numbers < 1){
+                alert('Introduce a number before an operator');
+                break;
+            }
         case '+':
         case '-':
            
