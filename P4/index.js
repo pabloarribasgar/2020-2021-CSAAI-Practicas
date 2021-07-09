7759924f0cca0e1859dca41ab748673689661376
pablo arribas <pabloarribas96@hotmail.com>
2021-07-09 12:41:07 +0200

    añadiendo img al principio

--- P4/index.js
+++ P4/index.js
@@ -1,6 +1,7 @@
 function start (){
     enableButtons();
     enableSliders();
+    drawImage();
     document.getElementsByClassName('canvasButton')[0].classList.add('hidden');
     document.getElementsByClassName('canvas')[0].classList.remove('hidden');
 }
