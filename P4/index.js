cb28fe4923031688c3e4d44c2bcf8485b12ad311
pablo arribas <pabloarribas96@hotmail.com>
2021-07-09 11:41:28 +0200

    corrigiendo errrores y añadiendo activacion

--- P4/index.js
+++ P4/index.js
@@ -1,12 +1,12 @@
 function start (){
-    
+    enableButtons();
     document.getElementsByClassName('canvasButton')[0].classList.add('hidden');
     document.getElementsByClassName('canvas')[0].classList.remove('hidden');
 }
 
 function enableButtons (){
     const buttons = document.getElementsByClassName('actionButton');
-    for (let i=0;i<buttons.length;i+){
+    for (let i=0;i<buttons.length;i++){
       buttons[i].removeAttribute('disabled'); 
     }
 }
