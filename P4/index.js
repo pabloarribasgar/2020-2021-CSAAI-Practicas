fad31faa45b00c1543a2d6c9d03182a472c1bedd
pablo arribas <pabloarribas96@hotmail.com>
2021-07-09 11:39:55 +0200

    añadiendo el activador

--- P4/index.js
+++ P4/index.js
@@ -3,3 +3,10 @@ function start (){
     document.getElementsByClassName('canvasButton')[0].classList.add('hidden');
     document.getElementsByClassName('canvas')[0].classList.remove('hidden');
 }
+
+function enableButtons (){
+    const buttons = document.getElementsByClassName('actionButton');
+    for (let i=0;i<buttons.length;i+){
+      buttons[i].removeAttribute('disabled'); 
+    }
+}
