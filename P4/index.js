b4cf04b7a43ee3af08a4f57c41545401e81727a6
pablo arribas <pabloarribas96@hotmail.com>
2021-07-09 11:39:02 +0200

    statr function

---
+++ P4/index.js
@@ -0,0 +1,5 @@
+function start (){
+    
+    document.getElementsByClassName('canvasButton')[0].classList.add('hidden');
+    document.getElementsByClassName('canvas')[0].classList.remove('hidden');
+}
